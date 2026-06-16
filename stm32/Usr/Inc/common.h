7f26787e1aee191742562175fc13346f7f7a5e04
Eric Handley <eric.a.handley@protonmail.com>
2026-06-15 22:20:17 -0700

    Update config for STM32U037

---
+++ stm32/Usr/Inc/common.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "stm32u0xx_hal.h"
+#include "stm32u0xx_hal_i2c.h"
+#include "stm32u0xx_hal_uart.h"
+
+#include "app_threadx.h"
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
