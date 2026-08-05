525da809cb4351a8a1922b10ab67dc14576f59bf
Eric Handley <eric.a.handley@protonmail.com>
2026-08-04 18:32:36 -0700

    Add driver for controlling camera from flight computer

---
+++ stm32/External/camera_driver.c
@@ -0,0 +1,54 @@
+#include "camera_driver.h"
+
+static UART_HandleTypeDef *__camera_e_to_huart(camera_e cam)
+{
+    switch (cam) {
+        case CAM1: return &CAM_1_UART;
+        case CAM2: return &CAM_2_UART;
+        default:   return NULL;
+    }
+}
+
+static cam_status_t __camera_send_cmd(camera_e cam, cam_command_t cmd, uint32_t timeout_ms)
+{
+    UART_HandleTypeDef *uart = __camera_e_to_huart(cam);
+    if (uart == NULL) {
+        return REPLY_ERROR;
+    }
+
+    // Discard anything already sitting in the receiver before sending a new command
+    // Otherwise replies that are recieved after the timeout from a previous command 
+    // will be used instead of the new reply
+    __HAL_UART_CLEAR_FLAG(uart, UART_CLEAR_OREF | UART_CLEAR_FEF | UART_CLEAR_NEF | UART_CLEAR_PEF);
+    __HAL_UART_SEND_REQ(uart, UART_RXDATA_FLUSH_REQUEST);
+
+    if (HAL_UART_Transmit(uart, (uint8_t *)&cmd, sizeof(cmd), timeout_ms) != HAL_OK) {
+        return REPLY_ERROR;
+    }
+
+    cam_status_t reply;
+    if (HAL_UART_Receive(uart, (uint8_t *)&reply, sizeof(reply), timeout_ms) != HAL_OK) {
+        return REPLY_ERROR;
+    }
+
+    if (reply > REPLY_INVALID_CMD) {
+        return REPLY_ERROR;
+    }
+
+    return reply;
+}
+
+cam_status_t camera_start(camera_e cam)
+{
+    return __camera_send_cmd(cam, CMD_START_CAM, CAM_UART_START_TIMEOUT_MS);
+}
+
+cam_status_t camera_stop(camera_e cam)
+{
+    return __camera_send_cmd(cam, CMD_STOP_CAM, CAM_UART_TIMEOUT_MS);
+}
+
+cam_status_t camera_status(camera_e cam)
+{
+    return __camera_send_cmd(cam, CMD_GET_STATUS, CAM_UART_TIMEOUT_MS);
+}
---
+++ stm32/External/camera_driver.h
@@ -0,0 +1,56 @@
+#pragma once
+
+// TODO Replace with correct HAL
+// #include "stm32u0xx_hal.h"
+// #include "stm32u0xx_hal_uart.h"
+
+// TODO Replace with correct UART handles
+// UARTs must be set to 115200 baud
+// extern UART_HandleTypeDef huart1;
+// extern UART_HandleTypeDef huart2;
+
+// TODO Correct UART handles here also
+#define CAM_1_UART huart1
+#define CAM_2_UART huart2
+
+// Start commands may take up to ~3.1s because we retry several times before
+// reporting an error. Other commands respond immediately
+#define CAM_UART_TIMEOUT_MS       100
+#define CAM_UART_START_TIMEOUT_MS 4000
+
+// FC -> CAM: commands from the flight computer.
+typedef enum __attribute__((packed)) {
+    CMD_START_CAM  = 1, // Start recording, will automatically record for the full duration set on the camera boards
+    CMD_STOP_CAM   = 2, // Disable recording + power down SoC, only used for error recovery from Ground Station
+    CMD_GET_STATUS = 3, // Get current cam_status_t from camera
+} cam_command_t;
+
+// CAM -> FC: responses to the flight computer.
+typedef enum __attribute__((packed)) {
+    REPLY_STOPPED     = 0, // Camera either has not started recording or is finished recording
+    REPLY_RECORDING   = 1, // Camera is recording
+    REPLY_ERROR       = 2, // Something has gone wrong! Cameras will try to recover from this automatically but new STOP/START may be needed in some cases
+    REPLY_BUSY        = 3, // Camera is in the middle of handling an existing command
+    REPLY_STARTING    = 4, // Powered on, SoC still booting / not yet recording
+    REPLY_INVALID_CMD = 5
+} cam_status_t;
+
+// enum for determining which camera should receive commands
+typedef enum {
+    CAM1,
+    CAM2
+} camera_e;
+
+// Double check that packets have the correct size so they can be decoded by cameras
+_Static_assert(sizeof(cam_command_t)    == 1, "cam_command_t must be 1 byte");
+_Static_assert(sizeof(cam_status_t)     == 1, "cam_status_t must be 1 byte");
+
+// Start camera c. Should return cam_status_t::REPLY_STARTING
+// Should then check status periodically until cam_status_t::REPLY_RECORDING is seen
+cam_status_t camera_start  ( camera_e c );
+
+// Stop camera c. Should return cam_status_t::REPLY_STOPPED
+cam_status_t camera_stop   ( camera_e c );
+
+// Get camera c status
+cam_status_t camera_status ( camera_e c );
